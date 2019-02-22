5bda7a762ac375aa2f070fcd44ae84c19a858f69
Teffisk <brad.mcknight91@gmail.com>
2019-02-22 12:17:45 -0800

    added slider formatting

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 
 const Header = (props) => { 
-  const wrapperStyle = { width: 400, margin: 50 };
+  const wrapperStyle = { width: 400, margin: 0 };
   const selected = "f4 lh-copy h3-5 ttu fw5 link dim ba bw1 ph3 pb3 pt3 mb2 dib black bg-white pointer selected";
   const notSelected = "f4 lh-copy h3 ttu fw5 link dim ph3 pb3 pt3 mb2 dib white bg-black pointer";
 
@@ -25,19 +25,21 @@ const Header = (props) => {
           </div>
         ))}
       </nav>
-      <div style={wrapperStyle}>
-        <div className="slider-container">
-          <p>1990</p> 
-          <Slider
-            className="slider"
-            onChange={props.handleChangeYear}
-            min={2011}
-            max={2013}
-            defaultValue={2013}
-            step={1}
-          /> 
-          <p>2013</p>
+      <div className="slider-section">
+        <span>1990</span> 
+        <div className="slider-parent" style={wrapperStyle}>
+          <div className="slider-container">
+            <Slider
+              className="slider"
+              onChange={props.handleChangeYear}
+              min={2011}
+              max={2013}
+              defaultValue={2013}
+              step={1}
+            /> 
+          </div>
         </div>
+        <span>2013</span>
       </div>
     </header>
   )
